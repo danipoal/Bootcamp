c8d40b809e1ab4f52f717146cce77bd41ce32b57
Daniel Alvarez <danipoal.0@gmail.com>
2024-06-25 19:57:14 +0200

    Inicio de piedra papel tijera en Typescript

---
+++ TypeScript/piedra-papel-tijera/main.js
@@ -0,0 +1,96 @@
+//LOGICA DE PIEDRA PAPEL TIJERA
+var tipos = ["piedra", "papel", "tijeras"];
+var Jugador = /** @class */ (function () {
+    function Jugador() {
+        this.eleccion = '';
+        this.puntuacion = 0;
+        this.ia = true;
+    }
+    Jugador.prototype.jugada = function () {
+        if (this.ia) {
+            return tipos[Math.floor(Math.random() * 3)];
+        }
+        else {
+            do { //Hacemos que mientras no se tenga la elección se compruebe todo el rato si en el buffer hay eleccion y luego se elija
+                updtConsoleOutput("Elige tu elección [piedra, papel, tijeras]");
+                if (buffer == "piedra" || buffer == "papel" || buffer == "tijeras") {
+                    this.eleccion = buffer;
+                }
+            } while (!this.eleccion);
+            return this.eleccion;
+        }
+    };
+    return Jugador;
+}());
+var comparacionGame = function (jugada1, jugada2) {
+    if (jugada1 === jugada2) {
+        return "Empate";
+    }
+    else if ((jugada1 === "piedra" && jugada2 === "tijeras") ||
+        (jugada1 === "papel" && jugada2 === "piedra") ||
+        (jugada1 === "tijeras" && jugada2 === "papel")) {
+        return "Gana jugador 1";
+    }
+    else {
+        return "Gana jugador 2";
+    }
+};
+//Canvas
+var canvas = document.getElementById('canvas');
+canvas.innerHTML = "que";
+//CONSOLA
+var consoleOutput = document.getElementById('output');
+var consoleInput = document.getElementById('input');
+var buffer = "";
+function updtConsoleOutput(message) {
+    if (consoleOutput) {
+        consoleOutput.innerText = message;
+    }
+}
+function controllerInput() {
+    if (consoleInput.value) {
+        var userInput = consoleInput.value; //Guardamos el valor en una constante
+        consoleInput.value = ""; //Reiniciamos el input para que cuando se pulse enter se borre el valor
+        return userInput;
+    }
+    else {
+        console.log("Error al introducir input");
+        throw new Error("Error: No hay valor en consoleInput");
+        //TODO Añadir algo rojo para indicar el error
+    }
+}
+consoleInput.addEventListener('keydown', function (evento) {
+    if (evento.key === "Enter") {
+        buffer = controllerInput();
+        validarInput(buffer);
+        console.log(buffer);
+    }
+});
+//Logica Partida
+function startGame(jugador1, jugador2) {
+    console.log(buffer);
+    canvas.innerHTML = "<p>Que empieze el juego! 🎰🕹</p>";
+    updtConsoleOutput("Quieres jugar vs IA [Si, No]");
+    console.log("Empezamos");
+    for (var index = 0; index < 6; index++) {
+        var jugada1 = jugador1.jugada();
+        var jugada2 = jugador2.jugada();
+        //Comparar el game
+        var result = comparacionGame(jugada1, jugada2);
+        canvas.innerHTML += "<p>" + result + "</p>";
+    }
+}
+function validarInput(buffer) {
+    if (buffer === "si") {
+        jugador2.ia = false;
+        console.log("Juego iniciado sin ia");
+    }
+    else if (consoleInput.value === "no") {
+        jugador2.ia = true;
+        console.log("Juego iniciado con ia");
+    }
+}
+var jugador1 = new Jugador();
+var jugador2 = new Jugador();
+startGame(jugador1, jugador2);
+//Estadisticas
---
+++ TypeScript/piedra-papel-tijera/main.ts
@@ -0,0 +1,117 @@
+//LOGICA DE PIEDRA PAPEL TIJERA
+const tipos = ["piedra", "papel", "tijeras"];
+
+class Jugador {
+    puntuacion : number;
+    eleccion : string;
+    ia: boolean;
+
+    constructor(){
+        this.eleccion = '';
+        this.puntuacion = 0;
+        this.ia = true;
+    }
+    jugada() : string  {
+        if(this.ia){
+            
+            return tipos[Math.floor(Math.random() * 3)];
+        }else{
+            do{ //Hacemos que mientras no se tenga la elección se compruebe todo el rato si en el buffer hay eleccion y luego se elija
+            
+                updtConsoleOutput("Elige tu elección [piedra, papel, tijeras]");
+            if(buffer == "piedra" || buffer == "papel" || buffer == "tijeras"){
+                this.eleccion = buffer;
+            }
+            }while(!this.eleccion)
+
+            
+            return this.eleccion;
+        }
+    }
+}
+const comparacionGame = (jugada1, jugada2) => {
+    if(jugada1 === jugada2){
+        return "Empate";
+    }else if(
+        (jugada1 === "piedra" && jugada2 === "tijeras") ||
+        (jugada1 === "papel" && jugada2 === "piedra") ||
+        (jugada1 === "tijeras" && jugada2 === "papel")
+    ) {
+        return "Gana jugador 1";
+    } else {
+        return "Gana jugador 2";
+    }
+}
+
+//Canvas
+
+const canvas = document.getElementById('canvas') as HTMLDivElement;
+
+canvas.innerHTML = "que";
+
+
+
+//CONSOLA
+let consoleOutput = document.getElementById('output') as HTMLParagraphElement;
+let consoleInput = document.getElementById('input') as HTMLInputElement;
+let buffer: string = "";
+
+function updtConsoleOutput(message: string) {
+    if (consoleOutput) {
+        consoleOutput.innerText = message;
+    }
+}
+function controllerInput() : string {        //Logica ejecutable al pulsar ENTER, la función ira en el eventListener
+    if (consoleInput.value) {
+        const userInput : string = consoleInput.value;       //Guardamos el valor en una constante
+        consoleInput.value = "";                    //Reiniciamos el input para que cuando se pulse enter se borre el valor
+        return userInput;
+    }else{ 
+        console.log("Error al introducir input")
+        throw new Error("Error: No hay valor en consoleInput");    
+        //TODO Añadir algo rojo para indicar el error
+    }
+}
+consoleInput.addEventListener('keydown', (evento) => {
+    if(evento.key === "Enter"){
+        buffer = controllerInput();
+        validarInput(buffer);
+        console.log(buffer);
+    }
+})
+
+//Logica Partida
+function startGame(jugador1 : Jugador, jugador2 : Jugador)  {
+    console.log(buffer);
+    canvas.innerHTML = "<p>Que empieze el juego! 🎰🕹</p>";
+
+    updtConsoleOutput("Quieres jugar vs IA [Si, No]");
+    console.log("Empezamos");
+
+
+    for (let index = 0; index < 6; index++) {
+        const jugada1 = jugador1.jugada();
+        const jugada2 = jugador2.jugada();
+        //Comparar el game
+        const result = comparacionGame(jugada1, jugada2);
+        canvas.innerHTML += "<p>" + result + "</p>";
+        
+    }
+
+}
+function validarInput(buffer){
+    if(buffer === "si"){
+        jugador2.ia = false;
+        console.log("Juego iniciado sin ia")
+    }else if(consoleInput.value === "no"){
+        jugador2.ia = true;
+        console.log("Juego iniciado con ia")
+
+    }
+}
+let jugador1 = new Jugador();
+let jugador2 = new Jugador();
+
+startGame(jugador1, jugador2);
+
+//Estadisticas
