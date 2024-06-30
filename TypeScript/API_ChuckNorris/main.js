ef5a2ceb2de9bb5c1a761c8589caa3df6c4c3a5e
Daniel Alvarez <danipoal.0@gmail.com>
2024-06-30 18:43:21 +0200

    Fetching Chuck Norris listo

--- TypeScript/API_ChuckNorris/main.js
+++ TypeScript/API_ChuckNorris/main.js
@@ -1,16 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("nes.css/css/nes.min.css");
-var jsonResponse = null; //Declaramos el objeto de la interfaz con todo null de momento
+var jsonResponse = null; // Declaramos el objeto de la interfaz con todo null de momento
+var botonFetch = document.querySelector(".boton-fetch");
+var textParragraph = document.querySelector("#p-objective");
 var requestOptions = {
     method: "GET",
     redirect: "follow"
 };
-fetch("https://api.chucknorris.io/jokes/random", requestOptions)
-    .then(function (response) { return response.json(); })
-    .then(function (result) { return saveResult(result); })
-    .catch(function (error) { return console.error(error); });
+var fetchJoke = function () {
+    fetch("https://api.chucknorris.io/jokes/random", requestOptions)
+        .then(function (response) { return response.json(); })
+        .then(function (result) {
+        saveResult(result);
+        console.log("click y fetch");
+        textParragraph.innerText = result.value;
+    })
+        .catch(function (error) { return console.error(error); });
+};
 var saveResult = function (json) {
     jsonResponse = json;
     console.log(jsonResponse);
 };
+botonFetch.addEventListener("click", fetchJoke);
--- TypeScript/API_ChuckNorris/main.ts
+++ TypeScript/API_ChuckNorris/main.ts
@@ -1,32 +1,39 @@
-import "nes.css/css/nes.min.css";
-
-//La interfaz es opcional de typescript, pero asi es la estructura de lo que devuelve la API
+// La interfaz es opcional de TypeScript, pero así es la estructura de lo que devuelve la API
 interface JokeResponse {
-    categories: string[];
-    created_at: string;
-    icon_url: string;
-    id: string;
-    updated_at: string;
-    url: string;
-    value: string;
-  }
-  
-let jsonResponse:JokeResponse | null = null;    //Declaramos el objeto de la interfaz con todo null de momento
+  categories: string[];
+  created_at: string;
+  icon_url: string;
+  id: string;
+  updated_at: string;
+  url: string;
+  value: string;
+}
+
+let jsonResponse: JokeResponse | null = null; // Declaramos el objeto de la interfaz con todo null de momento
+const botonFetch = document.querySelector(".boton-fetch") as HTMLButtonElement;
+const textParragraph = document.querySelector("#p-objective") as HTMLParagraphElement;
+
 
 const requestOptions: RequestInit = {
-    method: "GET",
-    redirect: "follow" as RequestRedirect 
-  };
+  method: "GET",
+  redirect: "follow" as RequestRedirect
+};
 
-  
+const fetchJoke = () => {
   fetch("https://api.chucknorris.io/jokes/random", requestOptions)
     .then((response) => response.json())
-    .then((result: JokeResponse) => saveResult(result))
+    .then((result: JokeResponse) => {
+      saveResult(result);
+      console.log("click y fetch")
+      textParragraph.innerText = result.value;
+    })
     .catch((error) => console.error(error));
+};
+
+const saveResult = (json: JokeResponse) => {
+  jsonResponse = json;
+  console.log(jsonResponse);
+};
 
 
-    const saveResult = (json : JokeResponse) => {
-        jsonResponse = json;
-        console.log(jsonResponse);
-
-    }    
+botonFetch.addEventListener("click", fetchJoke);
