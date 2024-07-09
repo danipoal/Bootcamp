8cdaf62f56e0dd6780bdf29e7097a7492f735626
Daniel Alvarez <danipoal.0@gmail.com>
2024-07-09 18:56:51 +0200

    Funcionamiento correcto fetch + boton añadir

--- Bootstrap/realBnb/main.js
+++ Bootstrap/realBnb/main.js
@@ -1,4 +1,13 @@
-//Login
+import { columna } from "./constantes.js";
+
+//VARIABLES
+const addCardBtn = document.querySelector('#addCard');
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+}
+
+//FUNCIONES
 document.addEventListener('DOMContentLoaded', function() {
     // Selecciona el formulario por su id
     const form = document.getElementById('loginForm');
@@ -18,41 +27,65 @@ document.addEventListener('DOMContentLoaded', function() {
         // Aquí podrías realizar alguna acción como enviar los datos a un servidor
     });
 });
-const cards = document.querySelectorAll('.card');
+addCardBtn.addEventListener('click', () => {
+    const main = document.querySelector('main');
+    main.innerHTML += columna; 
 
-const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-}
+    fetchPokemon();
+})
+
+/*
+* Se hace el fetch para cada elemento de carta html que exista. 
+*/
 const fetchPokemon = () => {
-    console.log(cards.length)
-
-    cards.forEach( (valor,index) => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach( (card,index) => {
+        //Cada Carta hace fetch a su propia url en orden de pokemons
         const indexPokemon = index + 1;
         const url = "https://pokeapi.co/api/v2/pokemon/" + indexPokemon;
 
-    })
+        fetch(url, requestOptions)
+        .then((response) => response.json())
+        .then((pokemon) => {
+            //textParragraph.innerText = result.value;
+            getImage(card, pokemon);
+            writeText(card, pokemon);
 
-    fetch("https://pokeapi.co/api/v2/pokemon/1", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        writeCard(result);
-        //console.log("click y fetch")
-        //textParragraph.innerText = result.value;
-      })
-      .catch((error) => console.error(error));
+        }).catch((error) => console.error(error));
+    })
   };
-
-const writeCard = (pokemon) => {
-    cards.forEach( card => {
-        //Para poner la imagen
-        const imagen = card.querySelector('.img-fluid');
-        imagen.src = pokemon.sprites.front_default;
-        console.log(pokemon.sprites.front_default);
-        console.log(imagen);
-    })
-    
+/*
+* Se coloca la imagen de el json en el src de la carta
+* @param cardObject - Es el elemento de el array de cards que identifica cada carta
+* @param pokemonJson - La información fetcheada de la api
+*/
+const getImage = (cardObject, pokemonJson) => {
+    const imagen = cardObject.querySelector('.img-fluid');
+    imagen.src = pokemonJson.sprites.front_default;
 }
+const writeText = (cardObject, pokemonJson) => {
+    //Para el nombre
+    const titulo = cardObject.querySelector('h3');
+    titulo.innerText = pokemonJson.name.toUpperCase();
+    //Para el tipo
+    const tipo = cardObject.querySelector('h6');
+    tipo.innerText = firstCamel(pokemonJson.types[0].type.name);
+    //Habilidades
+    const habilidadesText = cardObject.querySelector('#description');
+    habilidadesText.innerText = ""; //Se formatea para que no haya nada
+    pokemonJson.abilities.forEach(valor => { //Se añaden una a una con salto de linea
+        habilidadesText.innerText += firstCamel(valor.ability.name) + "\n";
+    })
+    //Peso
+    const peso = cardObject.querySelector(".n-habitaciones");
+    peso.innerText =pokemonJson.weight + " Kg";
+    console.log(columna);
+}
+function firstCamel(input){
+
+    return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+//LOGICA 
   fetchPokemon();
   
