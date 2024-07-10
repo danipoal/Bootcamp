cb65f35236605ccce7a5e3a34f485708e6b7759c
Daniel Alvarez <danipoal.0@gmail.com>
2024-07-10 19:52:08 +0200

    Ejercicio sql

---
+++ SQL/ejercicio1.sql
@@ -0,0 +1,9 @@
+-- EJERCICIO 1 : https://gist.github.com/DavidBernalGonzalez/b31524c01cb40028841292fa4a25be61
+-- SELECT * FROM myTable WHERE phone LIKE '(811)%';
+-- SELECT * FROM myTable WHERE country = 'Italy' OR country = 'Spain';
+-- SELECT count(id) FROM myTable;
+-- SELECT * FROM myTable WHERE id IN (SELECT id FROM myTable WHERE country = 'Germany' OR country = 'Turkey');
+-- SELECT max(id), min(id) FROM myTable;
+-- SELECT count(id), country FROM mytable GROUP BY country;
+-- SELECT * FROM mytable ORDER BY postalZip ASC LIMIT 10;Ç
+-- DELETE FROM mytable WHERE country = 'Singapore';	
